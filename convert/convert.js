525233d21e716fa177f1f112aae7046f28332913
nataliaoliveira9 <natalia@live.jp>
2022-06-17 23:44:32 -0300

    new layout

--- convert/convert.js
+++ convert/convert.js
@@ -2,8 +2,21 @@ function numToDec() {
 	let num = document.getElementById('num_to_dec').value
 	let base = parseInt(document.querySelector('input[name=base_a]:checked').value)
 	let dec = parseInt(num, base)
+
 	document.getElementById('num_to_dec_result').value = dec
 }
+
+document.getElementById('bin1').addEventListener('click', () => {
+		document.getElementById('num_to_dec').placeholder = 'binário'
+	})
+
+document.getElementById('oct1').addEventListener('click', () => {
+		document.getElementById('num_to_dec').placeholder = 'octal'
+	})
+
+document.getElementById('hex1').addEventListener('click', () => {
+		document.getElementById('num_to_dec').placeholder = 'hexadecimal'
+	})
 
 function decToNum() {
 	let dec = parseInt(document.getElementById('dec_to_num').value)
@@ -12,9 +25,14 @@ function decToNum() {
 	document.getElementById('dec_to_num_result').value = num
 }
 
-  $( function() {
-    $( "#accordion" ).accordion({
-      active: false,
-      collapsible: true
-    });
-  });
+document.getElementById('bin2').addEventListener('click', () => {
+		document.getElementById('dec_to_num_result').placeholder = 'binário'
+	})
+
+document.getElementById('oct2').addEventListener('click', () => {
+		document.getElementById('dec_to_num_result').placeholder = 'octal'
+	})
+
+document.getElementById('hex2').addEventListener('click', () => {
+		document.getElementById('dec_to_num_result').placeholder = 'hexadecimal'
+	})
