06a73f300a0f53f7a95c00f0ff338439a1f873c2
Natália Oliveira <natyoliveira@proton.me>
2024-11-24 01:45:57 -0300

    munich-candles

---
+++ munich-candles/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{b as a}from"./chunk-LAV6WKDA.mjs";import"./chunk-VBPELLMC.mjs";import"./chunk-WRO5WEVO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
