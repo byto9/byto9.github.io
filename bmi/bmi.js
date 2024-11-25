3d1ef0652b1e3ec14efb92cd28b46bd6b899ab5a
Natália Oliveira <natyoliveira@proton.me>
2024-11-25 14:45:03 -0300

    bmi

---
+++ bmi/bmi.js
@@ -0,0 +1,57 @@
+function getBMI() {
+  const i = document.getElementById("age").value;
+  const a = document.getElementById("height").value;
+  const p = document.getElementById("weight").value;
+  const result1 = document.getElementById("result1");
+  const result2 = document.getElementById("result2");
+  const result3 = document.getElementById("result3");
+
+  const bmi = (p / a ** 2).toFixed(2);
+  result1.innerHTML = `Your BMI is ${bmi} kg/m<sup>2</sup>`;
+
+  if (i >= 65) {
+    if (bmi > 0) {
+      bmi < 23
+        ? (result2.innerHTML = "Underweight")
+        : bmi >= 23 && bmi <= 30
+        ? (result2.innerHTML = "Normal weight")
+        : bmi > 30
+        ? (result2.innerHTML = "Overweight")
+        : (result2.innerHTML = "Invalid data!");
+    } else {
+      result2.innerHTML = "Invalid data!";
+    }
+  }
+  if (i >= 18 && i <= 64) {
+    if (bmi > 0) {
+      bmi < 18.4
+        ? (result2.innerHTML = "Underweight")
+        : bmi >= 18.5 && bmi <= 24.9
+        ? (result2.innerHTML = "Normal weight")
+        : bmi >= 25 && bmi <= 29.9
+        ? (result2.innerHTML = "Overweight")
+        : bmi >= 30 && bmi <= 34.9
+        ? (result2.innerHTML = "Obesity Grade I")
+        : bmi >= 35 && bmi <= 39.9
+        ? (result2.innerHTML = "Obesity Grade II")
+        : bmi >= 40
+        ? (result2.innerHTML = "Obesity Grade III")
+        : (result2.innerHTML = "Invalid data!");
+    } else {
+      result2.innerHTML = "Invalid data!";
+    }
+  }
+
+  if (i < 20) {
+    result3.innerHTML =
+      "You must be 20 years or older to use this calculator.<br>Please use a pediatric BMI calculator.";
+  } else {
+    result3.innerHTML = "";
+  }
+
+  $(".bmi_table").show();
+}
+
+$(function () {
+  $(".bmi_table").hide();
+});
